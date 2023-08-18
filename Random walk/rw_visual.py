ff13d8baf280530204c3620f9ff7e4bc2edc30c0
Enimalojd <enderston8@gmail.com>
2023-08-18 16:51:08 +0400

    Случайное блуждание

--- Random walk/rw_visual.py
+++ Random walk/rw_visual.py
@@ -1,22 +1,24 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-while True:
-    # построение случайного блуждания
-    rw = RandomWalk()
-    rw.fill_walk()
+# построение случайного блуждания
+rw = RandomWalk(50000)
+rw.fill_walk()
 
-    # нанесение точек на диаграмму
-    plt.style.use('classic')
-    fig, ax = plt.subplots()
-    point_numbers = range(rw.num_point)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
-    #выделение первой и последней точки
-    ax.scatter(0, 0, color='green', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], color='red', s=100)
+# нанесение точек на диаграмму
+plt.style.use('classic')
+fig, ax = plt.subplots(figsize=(15, 9))
+point_numbers = range(rw.num_point)
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+#выделение первой и последней точки
+ax.scatter(0, 0, color='green', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], color='red', s=100)
+#удаление осей
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
 
-    plt.show()
+plt.savefig('image/rw_image.png')
 
-    keep_running = input('Make another walk? (y/n): ')
-    if keep_running == 'n':
-        break
+
+
+
