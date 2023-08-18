73445898360429881fc7b55007189cb62d92bb05
Enimalojd <enderston8@gmail.com>
2023-08-18 15:40:27 +0400

    График кубов с цветовой картой

--- Beginning/scatter_cube.py
+++ Beginning/scatter_cube.py
@@ -1,15 +1,14 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1,5001))
+x_values = list(range(1, 5001))
 y_values = [x*x*x for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
-ax.set_title('Cubes numbers', fontsize=15)
-ax.set_xlabel('Value', fontsize=7)
-ax.set_ylabel('Cube of value', fontsize=7)
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-
-plt.show()
+plt.savefig('cube_plot.png')
