afb059c1bd50a936599c6b75d212bab5e2614f51
Enimalojd <enderston8@gmail.com>
2023-08-18 15:15:42 +0400

    Цветовая карта(график темнеет при больших значениях)

--- Beginning/mpl_squares.py
+++ Beginning/mpl_squares.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.style as style
 
 input_valuus = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
+style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(input_valuus ,squares, linewidth=3)
 
