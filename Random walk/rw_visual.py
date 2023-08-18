2fa3ed5dfd17cdc5018a878d0c4257dfbe59d0d4
Enimalojd <enderston8@gmail.com>
2023-08-18 16:24:40 +0400

    Прорисовка первой точки зелёным цветом и последней точки красным цветом

--- Random walk/rw_visual.py
+++ Random walk/rw_visual.py
@@ -10,7 +10,10 @@ while True:
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_point)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds, edgecolor='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    #выделение первой и последней точки
+    ax.scatter(0, 0, color='green', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], color='red', s=100)
 
     plt.show()
 
