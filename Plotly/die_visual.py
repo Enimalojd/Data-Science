8cac6c32f624e24e9aa10430e6f335f7e42e65bf
Enimalojd <enderston8@gmail.com>
2023-08-19 15:45:14 +0400

    Визуализация броска D6 и D10 50000 раз

--- Plotly/die_visual.py
+++ Plotly/die_visual.py
@@ -3,14 +3,12 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline
 
 die1 = Die()
-die2 = Die()
+die2 = Die(10)
 
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die1.roll() + die2.roll()
     results.append(result)
-
-print(results)
 
 freqs = []
 max_result = die1.num_sides + die2.num_sides
@@ -24,5 +22,5 @@ data = [Bar(x=x_values, y=freqs)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout':my_layout}, filename='image/d6_d6.html')
+my_layout = Layout(title='Results of rolling D6 and D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout':my_layout}, filename='image/d6_d10.html')
