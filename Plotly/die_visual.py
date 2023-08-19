728f9f1fd266aa77717a53c46691c31505023a70
Enimalojd <enderston8@gmail.com>
2023-08-19 15:50:27 +0400

    3 кубика D6 1000 раз

--- Plotly/die_visual.py
+++ Plotly/die_visual.py
@@ -3,24 +3,25 @@ from plotly.graph_objs import Bar, Layout
 from plotly import offline
 
 die1 = Die()
-die2 = Die(10)
+die2 = Die()
+die3 = Die()
 
 results = []
-for roll_num in range(50000):
-    result = die1.roll() + die2.roll()
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll() + die3.roll()
     results.append(result)
 
 freqs = []
-max_result = die1.num_sides + die2.num_sides
-for value in range(2, max_result+1):
+max_result = die1.num_sides + die2.num_sides + die3.num_sides
+for value in range(3, max_result+1):
     feq = results.count(value)
     freqs.append(feq)
 
 #визуализация результатов
-x_values = list(range(2, max_result+1))
+x_values = list(range(3, max_result+1))
 data = [Bar(x=x_values, y=freqs)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling D6 and D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout':my_layout}, filename='image/d6_d10.html')
+my_layout = Layout(title='Results of rolling three D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='image/d6_d6_d6.html')
