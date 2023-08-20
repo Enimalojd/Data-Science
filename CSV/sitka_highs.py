bb1a78e05b9eee8a0e1da8386765577a4c66a960
Enimalojd <enderston8@gmail.com>
2023-08-20 17:47:06 +0400

    График температур в июле г.Ситка

--- CSV/sitka_highs.py
+++ CSV/sitka_highs.py
@@ -1,25 +1,28 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-filename = 'data/sitka_weather_2021_simple.csv'
+filename = 'data/sitka_weather_07-2021_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
-
-    highs = []
+    #чтение максимальных температур и запись их в список
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[4])
+        dates.append(current_date)
         highs.append(high)
-    print(highs)
 #нанесение данных на диаграмму
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 plt.title('Daily high temperatures on Sitka 2021 year', fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel('Temperatures', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
