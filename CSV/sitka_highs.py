a55fa625352c7c38e680232e1e4480b0de8e71c8
Enimalojd <enderston8@gmail.com>
2023-08-20 17:59:08 +0400

    Обработка ошибки, если вдруг не будет нужной даты

--- CSV/sitka_highs.py
+++ CSV/sitka_highs.py
@@ -13,17 +13,22 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[4])
-        low = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 #нанесение данных на диаграмму
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red', alpha=0.5)
 ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.5)
 plt.title('Daily high temperatures on Sitka july 2021 year', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
