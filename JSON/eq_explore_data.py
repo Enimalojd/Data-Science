76c8370e87fcf3b663c0fda4b6ef1040fa1d1264
Enimalojd <enderston8@gmail.com>
2023-08-21 16:29:51 +0400

    Больше информации на карте

--- JSON/eq_explore_data.py
+++ JSON/eq_explore_data.py
@@ -12,21 +12,35 @@ with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
 
 all_eq_dict = all_eq_data['features']
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_text = [], [], [], []
 for eq_dict in all_eq_dict:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_text.append(title)
 
 #нанесение данных на карту
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_text,
+    'marker': {
+            'size': [5*mag for mag in mags],
+            'color': mags,
+            'colorscale': 'Viridis',
+            'reversescale': True,
+            'colorbar': {'title': 'Magnitude'},
+    }
+}]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
-offline.plot(fig,filename='images/global_earthquakes.html')
+offline.plot(fig,filename='images/global_earthquakes30.html')
 
 print(len(all_eq_dict))
 print(mags[:10])
